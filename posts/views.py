b2978b5218299351d1aae549d29b7f293f89468a
eltinawh <eltinawh@yahoo.com>
2024-12-21 22:01:49 +0700

    getting posts objects with ajax

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from posts.models import Post
 
-# Create your views here.
+
+def post_list_and_create(request):
+    posts = Post.objects.all()
+    print(posts.count())
+    return render(request, "posts/main.html", {"posts": posts})
+
+def load_post_data_view(request):
+    posts = Post.objects.all()
+    data = []
+    for post in posts:
+        item = {
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "author": post.author.user.username
+        }
+        data.append(item)
+    return JsonResponse({"data": data})
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'hello world x2'})
